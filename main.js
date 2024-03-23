86d35a9c7da7ae4b0f2c8fd3dea7ae62198f1591
Islam Musabekov <islamchik3039@gmail.com>
2024-03-23 21:59:13 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+// !Задание 1
+// let num = -5;
+
+// if (num > 0) {
+//     console.log("Положительное")
+// } else if (num < 0) {
+//     console.log("Отрицательное")
+// };
+
+// !Задание 2
+// let num = 5;
+
+// if (num % 2 === 0) {
+//     console.log("Четное")
+// } else {
+//     console.log("Нечетное")
+// };
+
+
+// !Задание 3
+// let prim = {};
+
+// if (typeof prim === 'object') { 
+//     console.log("Объект является ссылочным типом данных")
+// } else {
+//     console.log("Тип " + typeof prim + " является примитивным типом данных")
+// };
+
+
+// !Задание 4
+// let person = {
+//     name: 'Islam',
+//     age: 18
+// };
+
+// if (person.age > 17) {
+//     console.log(`Привет, меня зовут ${person.name} и это мой первый код`)
+// } else {
+//     console.log(`Привет, меня зовут ${person.name} и я несовершеннолетний`)
+// };
+
+// !Задание 5
+// let name = "Islam"; 
+// let age = 18; 
+
+// if (typeof name === 'string' && typeof age === 'number') { 
+//     console.log("Все введено верно")
+// } else {
+//     console.log("Данные введены неверно")
+// }
+
+// !Задание 7
+// let lg = 'ru'; 
+
+// let message = lg === 'en' ? 'This is english' :
+//             lg === 'ru' ? 'Это русский' :
+//             lg === 'de' ? 'Das ist Deutsch' :
+//             lg === 'kg' ? 'Бул кыргыз тили' :
+//             'Неизвестный язык'; 
+// console.log(message);
+
+
+
+// !Задание 8
+// let person = {
+//     name: "Hello",
+//     age: 19,
+//     status: "student",
+//     class: "JS-54",
+//     school: "Makers"
+// };
+
+// if (person.age >= 20 && person.status === "student" && person.school === "Makers") {
+//     console.log(`Привет, меня зовут ${person.name} и я студент в ${person.school} классе ${person.class}`)
+// } else {
+//     console.log(`Привет, меня зовут ${person.name} и мне ${person.age} лет`)
+// };
+
+// !Задание 9
+// let minute = 35;
+
+// if (minute >= 1 && minute <= 15) {
+//     console.log("Число попадает в первую четверть часа")
+// } else if (minute > 15 && minute <= 30) {
+//     console.log("Число попадает во вторую четверть часа")
+// } else if (minute > 30 && minute <= 45) {
+//     console.log("Число попадает в третью четверть часа")
+// } else if (minute > 45 && minute <= 60) {
+//     console.log("Число попадает в четвертую четверть часа")
+// } else {
+//     console.log("Число не верное")
+// };
